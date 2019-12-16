79f1f8e8107adf727832f8d9773047cc5f501300
Tomas Kacur <tomas.kacur@keboola.com>
2019-12-16 22:47:38 +0100

    aks-netlab: uncomment resource creation

--- scripts/aks-networking-lab.sh
+++ scripts/aks-networking-lab.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-#./scripts/resource-group.sh
-#./scripts/network.sh
-#./scripts/aks-cluster.sh & ./scripts/vm-server.sh
+./scripts/resource-group.sh
+./scripts/network.sh
+./scripts/aks-cluster.sh & ./scripts/vm-server.sh
 
 echo "Private IP address of VM server:" $(az group deployment show --resource-group kacurez-pokus --name vm-server --query "properties.outputs.privateIPAddress.value"  --output tsv)
